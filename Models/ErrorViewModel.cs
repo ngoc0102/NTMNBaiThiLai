6c1f7095b7f5428106a3530ee543a046f77fe3a2
ngoc0102 <ngoc0102@gmail.com>
2024-06-20 23:33:31 +0700

    NTMNBaiThiLai

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NTMNBaiThiLai.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
